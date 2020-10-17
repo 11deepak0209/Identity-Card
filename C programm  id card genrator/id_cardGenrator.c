b7129f7ee6cbe8665dd059866cbd9ad44b24e9aa
vanshammishra <72139336+vanshammishra@users.noreply.github.com>
2020-10-18 00:32:24 +0530

    Update id_cardGenrator.c

--- C programm  id card genrator/id_cardGenrator.c
+++ C programm  id card genrator/id_cardGenrator.c
@@ -5,11 +5,11 @@
 #include <conio.h>
 struct id_Card
 {
-    int Name[100];  
+    int Name[69];  // saved the extra space which was used by array
     int company[100];
     int location[100];
 
-};struct id_Card e1,e2;
+};struct id_Card e1;
 
 
 int main()
@@ -24,14 +24,6 @@ int main()
     scanf("%s",&e1.company);
     printf("\tEnter the location or present adress of employe\n");
     scanf("%s",&e1.location);
-    // employe 2 Data
-    printf("\tEnter the information of employe 2 \n");
-    printf("\t Enter the name \n");
-    scanf("%s",&e2.Name);
-    printf("\tEnter the company Name of employe\n");
-    scanf("%s",&e2.company);
-    printf("\tEnter the location or present adress of employe\n");
-    scanf("%s",&e2.location);
 
      
     printf("           Genrating id card*** please wait \n\n");
@@ -55,28 +47,8 @@ int main()
      printf("%d",id[i]);
     i++;  
   }
-     //Employe Data 2 ouptut
- 
-    printf("\tEmploye data 2")
-    printf("\t Name   = %s\n",e2.Name);
-    printf("\t Company Name = %s\n",e2.company);
-    printf("\t Loaction = %s\n",e2.location);
-  for (int i = 0; i <10; i++)
-  {
-     
-     srand(time(NULL));
-     id[i]= rand()%10;
-    i++;  
-  
-      /* code */
-  }
-  
-  printf("\t EMploy id = ");
-  for (int i = 0; i <10; i++)
-  {
-     printf("%d",id[i]);
-    i++;  
-  } 
- 
+    
+   
+
     return 0;
 }
